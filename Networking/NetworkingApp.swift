5dc0397cf59414f79b60bd6f6c968d7a13f2b14d
Vladyslav Kostenko <v.kostenko@noisyminer.com>
2023-01-16 22:30:56 +0100

    Initial Commit

---
+++ Networking/NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingApp.swift
+//  Networking
+//
+//  Created by Vladyslav Kostenko on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
