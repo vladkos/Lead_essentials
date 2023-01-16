b78116b5f8b8a6cf232cbd3334bafe4a5d6b06d3
Vladyslav Kostenko <v.kostenko@noisyminer.com>
2023-01-16 23:20:04 +0100

    Asserting a Captured Value Is Not Enough + Cross-Module Access Control

---
+++ Networking/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  Networking
+//
+//  Created by Vladyslav Kostenko on 16/01/2023.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
