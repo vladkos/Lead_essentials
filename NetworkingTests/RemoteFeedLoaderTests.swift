1a691e95236aabcce1e52fa8dbbd249741774c29
Vladyslav Kostenko <v.kostenko@noisyminer.com>
2023-01-16 23:07:39 +0100

    From Singletons and Globals to Proper Dependency Injection

---
+++ NetworkingTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,58 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  NetworkingTests
+//
+//  Created by Vladyslav Kostenko on 16/01/2023.
+//
+
+import XCTest
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+final class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let (_, client) = makeSUT()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
+    }
+}
